4a1193f667e54407ae949eecded2ee1990ce050f
f0r3st <adgerom11@gmail.com>
2019-09-17 18:11:53 +0300

    fixes

--- src/pages/PageHome/desktop/PageHomeDesktop.js
+++ src/pages/PageHome/desktop/PageHomeDesktop.js
@@ -14,17 +14,17 @@ const PageHomeDesktop = () => (
     <Advantages />
     <Layout>
       <h2>Услуги эвакуатора</h2>
-      <div style={{ width: '75rem', marginBottom: '6rem' }}><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
+      <div style={{ width: '75rem', marginBottom: '6rem' }}><span>Неожиданные неириятности в дороге могут случиться с каждым. Любой, даже практически новый автомобиль, который своевременно обслуживается в СЦ, не застрахован от поломки. В подобной ситуации закажите эвакуатор! Мы гарантируем Вам быстрое прибытие в любое время, а также недорогие расценки и бережное отношение к авто.</span></div>
       <ul>
         <li><span>Эвакуация с места поломки или аварии</span></li>
         <li><span>Буксировка автомобиля</span></li>
-        <li><span>Перевозка машины по России</span></li>
+        <li><span>Перевозка машины по России от 40Р за километр</span></li>
       </ul>
     </Layout>
     <PriceBlock />
     <Layout>
       <h2>Услуги эвакуатора</h2>
-      <div style={{ width: '75rem', marginBottom: '8rem' }}><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
+      <div style={{ width: '75rem', marginBottom: '13rem' }}><span>Оперативная подача эвакуатора в Сыктывкаре и регионах, безопасная транспортировка автомобиля – вот наша главная задача. И уже несколько лет мы успешно справляемся с этой задачей.</span></div>
     </Layout>
     <Footer />
   </div>
